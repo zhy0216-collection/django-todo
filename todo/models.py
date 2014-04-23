a4dc806f1aaea0ada3a3d115477d5393b52d847d
Yang <zhyang@xiaomi.com>
2014-04-23 14:59:36 +0800

    make basic model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone 
 
 # Create your models here.
+class TodoList(models.Model):
+    name        = models.CharField()
+    # author      = models.ForeignKey("User")
+    create_time = models.DateTimeField(auto_now_add=True)
+
+
+    def __unicode__(self):
+        return self.name
+
+
+class TodoItem(models.Model):
+    content     = models.CharField()
+    todo_list   = models.ForeignKey(TodoItem)
+
+    def __unicode__(self):
+        return self.content
+
+
